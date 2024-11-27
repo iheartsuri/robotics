5e1da74d709c24dffb76139bb0dd940799eb7739
iheartsuri <lxv.riri@gmail.com>
2024-11-27 13:09:09 -0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+a=5
+b=6
+
+addition = a+b 
+
+subtraction = a-b
+
+multiplication = a*b 
+
+division = a/b 
+
+power = a**b
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+print(power)
